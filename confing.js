3f79d558c060eed84bf12866d7d5deac057dee5a
Rebira Adugna <rebikman9@gmail.com>
2025-03-06 09:30:27 +0300

    version last

--- confing.js
+++ confing.js
@@ -1,3 +1,4 @@
 console.log("version 1")
 console.log("updated")
+console.log("for checking something")
 
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 console.log("confing.js")
 console.log("version 2")
+console.log("for checking something")
+console.log("fire")
